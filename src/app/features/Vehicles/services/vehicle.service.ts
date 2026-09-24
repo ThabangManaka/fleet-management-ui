9c7be8cb1874957b7d3223199aa20428ee30c95a
Thabang Manaka <thabangdansteyn@gmail.com>
2026-09-24 21:31:06 +0200

    added vehicle serivices

--- src/app/features/Vehicles/services/vehicle.service.ts
+++ src/app/features/Vehicles/services/vehicle.service.ts
@@ -1,6 +1,30 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { Vehicle } from '../models/vehicle.model';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
-export class VehicleService {}
+export class VehicleService {
+
+  private readonly http = inject(HttpClient);
+
+  private readonly apiUrl =
+    'https://localhost:YOUR_API_PORT/api/Vehicles';
+
+  getVehicles(): Observable<Vehicle[]> {
+    return this.http.get<Vehicle[]>(this.apiUrl);
+  }
+
+  getVehicle(id: string): Observable<Vehicle> {
+    return this.http.get<Vehicle>(`${this.apiUrl}/${id}`);
+  }
+
+  getVehiclePerformance(id: string): Observable<VehiclePerformance> {
+    return this.http.get<VehiclePerformance>(
+      `${this.apiUrl}/${id}/performance`
+    );
+  }
+}
